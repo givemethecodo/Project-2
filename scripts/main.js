1227bf4a2ca54e293a741e0c1ef403838eec2dab
Erika <ehance@sfu.ca>
2022-11-18 06:09:22 -0800

    Product Listing Pages Fixed
    Updated copywriting, also made a proper main product listing page with 3 options to 3 different products

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,19 @@
 "use strict";
+
+// learned this here:  https:stackoverflow.com/questions/9186346/javascript-onclick-increment-number
+function incrementValue()
+{
+var value = parseInt(document.getElementById('product_qty').innerHTML, 10);
+console.log(value);
+value = isNaN(value) ? 0 : value;
+value++;
+document.getElementById('product_qty').innerHTML = value;
+}
+
+function decreaseValue()
+{
+var value = parseInt(document.getElementById('product_qty').innerHTML, 10);
+value = isNaN(value) ? 0 : value;
+value--;
+document.getElementById('product_qty').innerHTML = value;
+}
