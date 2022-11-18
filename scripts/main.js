7b9543a6724db734415ca9f91def46b3a3c0ab14
Erika <ehance@sfu.ca>
2022-11-18 08:25:26 -0800

    Standardized all pages
    All pages have same nav bar and footer, also spacing and responsiveness and some broken links have been fixed.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// learned this here:  https:stackoverflow.com/questions/9186346/javascript-onclick-increment-number
+// learned this here:  https://stackoverflow.com/questions/9186346/javascript-onclick-increment-number
 function incrementValue()
 {
 var value = parseInt(document.getElementById('product_qty').innerHTML, 10);
@@ -15,5 +15,8 @@ function decreaseValue()
 var value = parseInt(document.getElementById('product_qty').innerHTML, 10);
 value = isNaN(value) ? 0 : value;
 value--;
+if (value < 2) {
+  value = 1;
+}
 document.getElementById('product_qty').innerHTML = value;
 }
